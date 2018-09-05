46f6a6dcbe6c08f3636531b84d0616766146b7d1
efratzadok0879 <efrat.zadok@seldatinc.com>
2018-09-05 10:46:06 +0300

    book store app

--- front-end/book-store/src/app/shared/global.ts
+++ front-end/book-store/src/app/shared/global.ts
@@ -1,7 +1,7 @@
 
 export const Global =
 {
-    Host:"https://lit-temple-43730.herokuapp.com/",
+    Host:"https://lit-temple-43730.herokuapp.com",
     Uploads:"https://lit-temple-43730.herokuapp.com/uploads",
     CurrentUser: "currentUser",
     ShoppingList:"shoppingList"
